09561815fe3a1b7c09ea8b09a7cd55067afa9e32
civainu <civa11519@gmail.com>
2019-05-20 17:42:36 +0900

    非同期通信の結果を得て、HTMLを作成する

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -49,12 +49,24 @@ $('#new_message').on('submit', function(e) {
 })
 
 $('#user-search-field').on("keyup", function(){
-  var input = $('#user-search-field').val();
-  $.ajax({
-    type:  'GET',
-    url: '/users',
-    data: { keyword: input },
-    dataType: 'json',
+    var input = $('#user-search-field').val();
+    $.ajax({
+      type:  'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json',
+    })
+    .done(function(users) {
+      console.log(users)
+      $('#user-search-result').empty();
+      if (users.length !== 0 ) {
+      users.forEach(function(user){
+          appendUser(user);
+      })
+      }
+      else {
+        appendNoUser("一致するユーザーがいません")
+      }
+    })
+    
   })
-  
-})
