8727acda79fa9643fe490b2d5b19a0af3c503305
civainu <civa11519@gmail.com>
2019-05-21 18:39:31 +0900

    非同期通信の結果を得て、HTMLを作成する２

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -46,11 +46,25 @@ $('#new_message').on('submit', function(e) {
     $('.form__submit').prop('disabled', false);
  })
 })
-})
+
+  function appendUser(user) {
+    var html = `<div class="chat-group-user clearfix">
+      <p class="chat-group-user__name">${user.name}</p>
+      <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+     </div>`
+  $('#user-search-result').append(html);
+ }
+  function addUser(user_id, user_name){
+    var html = `<div class="chat-group-user clearfix">
+      <p class="chat-group-user__name">${user.name}</p>
+      <input name="chat_group[user_ids][]" type="hidden" value=${user.id}>
+      <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">削除</a>
+     </div>`
+  $('#chat-group-user').append(html);
+ }
 
 $('#user-search-field').on("keyup", function(){
   var input = $('#user-search-field').val();
-  console.log(input);
   $.ajax({
     type:  'GET',
     url: '/users',
@@ -58,11 +72,10 @@ $('#user-search-field').on("keyup", function(){
     dataType: 'json',
   })
   .done(function(users) {
-    console.log(users)
     $('#user-search-result').empty();
     if (users.length !== 0 ) {
     users.forEach(function(user){
-        appendUser(user);
+      appendUser(user);
     })
     }
     else {
@@ -72,4 +85,5 @@ $('#user-search-field').on("keyup", function(){
   .fail(function(){
     alert('ユーザー検索に失敗しました');
   })
+})
 })
