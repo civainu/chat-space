979eda9285649602c8c49847ccfe976cf9b1e80c
civainu <civa11519@gmail.com>
2019-05-21 12:56:09 +0900

    エラー時の処理

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -49,24 +49,26 @@ $('#new_message').on('submit', function(e) {
 })
 
 $('#user-search-field').on("keyup", function(){
-    var input = $('#user-search-field').val();
-    $.ajax({
-      type:  'GET',
-      url: '/users',
-      data: { keyword: input },
-      dataType: 'json',
-    })
-    .done(function(users) {
-      console.log(users)
-      $('#user-search-result').empty();
-      if (users.length !== 0 ) {
-      users.forEach(function(user){
-          appendUser(user);
-      })
-      }
-      else {
-        appendNoUser("一致するユーザーがいません")
-      }
-    })
-    
+  var input = $('#user-search-field').val();
+  $.ajax({
+    type:  'GET',
+    url: '/users',
+    data: { keyword: input },
+    dataType: 'json',
   })
+  .done(function(users) {
+    console.log(users)
+    $('#user-search-result').empty();
+    if (users.length !== 0 ) {
+    users.forEach(function(user){
+        appendUser(user);
+    })
+    }
+    else {
+      appendNoUser("一致するユーザーがいません")
+    }
+  })
+  .fail(function(){
+    alert('ユーザー検索に失敗しました');
+  })
+})
