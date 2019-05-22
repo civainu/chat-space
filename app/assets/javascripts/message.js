bf011be4ed1fa5a86d66ad6a8961a57ea32ea40f
civainu <civa11519@gmail.com>
2019-05-22 10:48:50 +0900

    削除機能追加

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -43,6 +43,9 @@ $('#new_message').on('submit', function(e) {
   .fail(function() {
     alert('メッセージの送信に失敗しました');
   })
+  .always(function(){
+    $(".input-box__button").removeAttr("disabled");
+  });
 })
 
   function appendUser(user) {
@@ -60,12 +63,13 @@ $('#new_message').on('submit', function(e) {
   $('#user-search-result').append(html);
 }
   function addUser(user_id, user_name){
-    var html = `<div class="chat-group-user clearfix">
-      <p class="chat-group-user__name">${user_name}</p>
-      <input name="chat_group[user_ids][]" type="hidden" value=${user_id}>
-      <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user_id}" data-user-name="${user_name}">削除</a>
-     </div>`
-  $('#chat-group-user').append(html);
+    var html = `<div class="chat-group-user clearfix js-chat-member" data-user-id="${user_id}">
+                  <p class="chat-group-user__name">${user_name}</p>
+                  <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn">削除</div>
+                  <input name="group[user_ids][]" type="hidden" id="group_user_ids" value="${user_id}">
+                </div>`
+     console.log(html);
+  $('#chat-group-users').append(html);
  }
 
 $('#user-search-field').on("keyup", function(){
